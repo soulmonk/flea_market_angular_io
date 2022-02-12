84317a8ade7c8a21d3153f66ce1d9d4ed22730c9
SoulMonk <fsoulmonk@gmail.com>
2022-02-12 12:16:23 +0100

    apollo-angular

--- src/app/finance-stats/graphql.module.ts
+++ src/app/finance-stats/graphql.module.ts
@@ -1,7 +1,10 @@
-import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLinkModule, HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+import {NgModule} from '@angular/core';
+
+
+
 import { environment } from '@env'
 
 const uri = environment.apiServers.financeStats + '/graphql';
