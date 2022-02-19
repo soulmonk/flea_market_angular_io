a587725ec0e86b3511413b66945f02fb7bb24b7b
SoulMonk <fsoulmonk@gmail.com>
2022-02-19 21:44:03 +0100

    update apollo

--- src/app/finance-stats/graphql.module.ts
+++ src/app/finance-stats/graphql.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {InMemoryCache} from '@apollo/client/core';
-import {Apollo} from 'apollo-angular';
+import {Apollo, APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
 import {HttpLink} from 'apollo-angular/http';
 import {environment} from '@env';
 import {setContext} from '@apollo/client/link/context';
@@ -8,44 +8,39 @@ import {AuthService} from '@app/auth/services/auth.service';
 
 const uri = environment.apiServers.financeStats + '/graphql';
 
-// export function createApollo(httpLink: HttpLink) {
-//   return {
-//     link: httpLink.create({uri}),
-//     cache: new InMemoryCache(),
-//   };
-// }
+export function createApollo(httpLink: HttpLink, authService: AuthService) {
+  const base = httpLink.create({
+    uri,
+  });
+  const authLink = setContext((_, {headers}) => {
+    // get the authentication token from local storage if it exists
+    const token = authService.getToken();
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      }
+    };
+  });
+  return {
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
+}
 
 @NgModule({
-  exports: [],
+  imports: [ApolloModule],
+  exports: [ApolloModule],
   providers: [
-    // { // todo not working wiht angular 13?
-    //   provide: APOLLO_OPTIONS,
-    //   useFactory: createApollo,
-    //   deps: [HttpLink],
-    // },
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink, AuthService],
+    },
     // ListTransactionsGQL,
   ],
 })
 export class GraphQLModule {
-  constructor(apollo: Apollo, httpLink: HttpLink, authService: AuthService) {
-    const base = httpLink.create({
-      uri,
-    });
-    const authLink = setContext((_, {headers}) => {
-      // get the authentication token from local storage if it exists
-      const token = authService.getToken();
-      // return the headers to the context so httpLink can read them
-      return {
-        headers: {
-          ...headers,
-          authorization: token ? `Bearer ${token}` : '',
-        }
-      };
-    });
-
-    apollo.create({
-      link: authLink.concat(base),
-      cache: new InMemoryCache()
-    });
-  }
+  constructor() {}
 }
